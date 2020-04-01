05d5604d87d2e01873f77b1ef08c295a20066f15
PabloMorixe <45852748+PabloMorixe@users.noreply.github.com>
2020-04-01 13:43:55 -0300

    Elimine contenido de jorge

--- jorge.php
+++ jorge.php
@@ -1,17 +1,1 @@
-prueba de subida jp
-
-sigo escribiendo para probar la sincronizac
-
-
-
-<<<<<<< HEAD
-
-
-JUAMPI PUTO 13:28 
-
-hola
-=======
->>>>>>> 483aea5fb0fb34e173600be1d635891d916adda5
-ddddd
-
-pablo 13:38
+Pablo 13:38
